cd714ad1b20865fa993b65d0a7b5e1e9ede8620c
Gavin <gavin.n.lobo@gmail.com>
2022-02-13 12:25:07 -0500

    Cleaned up a bit

--- main.cpp
+++ main.cpp
@@ -3,43 +3,43 @@
 #include <string>
 #include <map>
 #include <variant>
+#include <vulkan/vulkan.h>
 #include <unordered_set>
 #include <spdlog/spdlog.h>
+
+
+struct RenderTargetDefinition
+{
+    std::string name;
+    VkFormat    format = VK_FORMAT_UNDEFINED;
+    uint32_t    width  = 0;
+    uint32_t    height = 0;
+};
+
+struct ImageDefinition
+{
+    std::string name;
+    VkFormat    format = VK_FORMAT_UNDEFINED;
+    uint32_t    width  = 0;
+    uint32_t    height = 0;
+};
 
 
 struct RenderPassNode
 {
     std::string name;
-    std::string type;
-    std::vector<std::string> inputNodes;  // input render targets
-    std::vector<std::string> outputNodes; // output render targets
 
-    RenderPassNode& setOutputs(std::vector<std::string> const & outputTargets)
+    std::vector<RenderTargetDefinition> inputRenderTargets;  // input render targets
+    std::vector<RenderTargetDefinition> outputRenderTargets; // output render targets
+
+    RenderPassNode& input(std::string name)
     {
-        for(auto & i : outputTargets)
-        {
-            insertOutputNode(i);
-        }
+        inputRenderTargets.push_back({name});
         return *this;
     }
-
-    RenderPassNode& setInputs(std::vector<std::string> const & inputTargets)
+    RenderPassNode& output(std::string name, VkFormat format=VK_FORMAT_UNDEFINED)
     {
-        for(auto & i : inputTargets)
-        {
-            insertInputNode(i);
-        }
-        return *this;
-    }
-
-    RenderPassNode& insertOutputNode(std::string nodeName)
-    {
-        outputNodes.push_back(nodeName);
-        return *this;
-    }
-    RenderPassNode& insertInputNode(std::string nodeName)
-    {
-        inputNodes.push_back(nodeName);
+        outputRenderTargets.push_back({name,format});
         return *this;
     }
 };
@@ -50,7 +50,8 @@ struct RenderTargetNode
     std::string              writer;  // only one render pass can write to a render target
     std::vector<std::string> readers; // multiple render passes can read from this render target
 
-    std::string imageResource;
+    RenderTargetDefinition   imageResource;
+    //std::string imageResource;
 };
 
 
@@ -59,7 +60,26 @@ using node = std::variant<RenderPassNode,RenderTargetNode>;
 
 struct FrameGraph
 {
+
 #define BE(A) std::begin(A),std::end(A)
+    std::vector<std::string> findExecutionOrder() const
+    {
+        auto endNodes = findEndNodes();
+        std::vector<std::string> order;
+        for(auto & name : endNodes)
+        {
+            _recursePushBack(name, order);
+        }
+
+        std::reverse(BE(order));
+        std::unordered_set<std::string> seen;
+        auto it = std::remove_if(BE(order),[&seen](auto & x)
+        {
+           return seen.insert(x).second == false;
+        });
+        order.erase(it, order.end());
+        return order;
+    }
 protected:
     std::vector<std::string> findEndNodes() const
     {
@@ -87,9 +107,9 @@ protected:
         if(std::holds_alternative<RenderPassNode>(n))
         {
             auto & N = std::get<RenderPassNode>(n); // should always be a render pass node
-            for(auto & in : N.inputNodes)
+            for(auto & in : N.inputRenderTargets)
             {
-                _recursePushBack(in, order);
+                _recursePushBack(in.name, order);
             }
         }
         else if(std::holds_alternative<RenderTargetNode>(n))
@@ -100,34 +120,17 @@ protected:
         }
 
     }
-    std::vector<std::string> findExecutionOrder() const
-    {
-        auto endNodes = findEndNodes();
-        std::vector<std::string> order;
-        for(auto & name : endNodes)
-        {
-            _recursePushBack(name, order);
-        }
-
-        std::reverse(BE(order));
-        std::unordered_set<std::string> seen;
-        auto it = std::remove_if(BE(order),[&seen](auto & x)
-        {
-           return seen.insert(x).second == false;
-        });
-        order.erase(it, order.end());
-        return order;
-    }
-
 
     // returns the name of the render target which
     // has an image but is no longer being used
-    std::string findRenderTargetThatIsNotBeingUsed(std::map<std::string, int32_t> & renderTargetUsageCount)
+    std::string findImageThatIsNotBeingUsed(std::map<std::string, int32_t> & renderTargetUsageCount, RenderTargetDefinition const & def)
     {
         for(auto & [name, count] : renderTargetUsageCount)
         {
             if(count == 0)
             {
+                auto & N = std::get<RenderTargetNode>(nodes.at(name));
+                //auto & NN = std::get<RenderTargetNode>()
                 return name;
             }
         }
@@ -144,12 +147,12 @@ protected:
         for(auto & [name,n] : nodes)
         {
             auto & N = std::get<RenderPassNode>(n);
-            for(auto & o : N.outputNodes)
+            for(auto & o : N.outputRenderTargets)
             {
-                if( images.count(o) == 0)
+                if( images.count(o.name) == 0)
                 {
-                    auto & nI = images[o];
-                    nI.name   = o;
+                    auto & nI = images[o.name];
+                    nI.name   = o.name;
                     nI.writer = name;
                 }
             }
@@ -159,9 +162,9 @@ protected:
         for(auto & n : nodes)
         {
             auto & N = std::get<RenderPassNode>(n.second);
-            for(auto & in : N.inputNodes)
+            for(auto & in : N.inputRenderTargets)
             {
-                images.at(in).readers.push_back(n.first);
+                images.at(in.name).readers.push_back(n.first);
             }
         }
 
@@ -193,13 +196,13 @@ public:
 
         spdlog::info("Execute Order: {}", fmt::join(BE(order),","));
         //std::map<std::string, int32_t> imageUseCount;
-        std::map<std::string, int32_t> renderTargetUseCount;
+        std::map<std::string, int32_t> imageUseCount;
 
         auto _print = [&]()
         {
-            for(auto & [rtName, count] : renderTargetUseCount)
+            for(auto & [rtName, count] : imageUseCount)
             {
-                spdlog::info("{} : {}   {}", rtName, count, std::get<RenderTargetNode>(nodes.at(rtName)).imageResource);
+                spdlog::info("{} : {}   {}", rtName, count, std::get<RenderTargetNode>(nodes.at(rtName)).imageResource.name);
             }
             spdlog::info("----");
 
@@ -213,13 +216,13 @@ public:
             if( std::holds_alternative<RenderPassNode>(n) )
             {
                 auto & N = std::get<RenderPassNode>(n);
-                for(auto & n : N.outputNodes)
+                for(auto & n : N.outputRenderTargets)
                 {
-                    renderTargetUseCount[n]++;
+                    imageUseCount[n.name]++;
                 }
-                for(auto & n : N.inputNodes)
+                for(auto & n : N.inputRenderTargets)
                 {
-                    renderTargetUseCount[n]++;
+                    imageUseCount[n.name]++;
                 }
             }
         }
@@ -233,34 +236,44 @@ public:
 
             spdlog::info("Pass Name: {}", name);
 
-            for(auto & outputName : N.outputNodes)
+            for(auto & outTarget : N.outputRenderTargets)
             {
-                auto & outRenderTarget = std::get<RenderTargetNode>(nodes.at(outputName));
-                auto renderTargetThatIsNotBeingUsed = findRenderTargetThatIsNotBeingUsed(renderTargetUseCount);
+                auto & outRenderTarget = std::get<RenderTargetNode>(nodes.at(outTarget.name));
+                auto imageThatIsNotBeingUsed = findImageThatIsNotBeingUsed(imageUseCount, outTarget);
 
-                if(renderTargetThatIsNotBeingUsed.empty()) // no available image
+                if(imageThatIsNotBeingUsed.empty()) // no available image
                 {
                     // generate new image
-                    outRenderTarget.imageResource = fmt::format("{}_img", outputName);
+                    auto imageName = fmt::format("{}_img", outTarget.name);
+                    outRenderTarget.imageResource.name   = imageName;
+                    outRenderTarget.imageResource.format = outTarget.format;
+
+                    ImageDefinition imgDef;
+                    imgDef.name   = imageName;
+                    imgDef.format = outTarget.format;
+                    imgDef.width  = outTarget.width;
+                    imgDef.height = outTarget.height;
+
+                    m_images[imageName] = imgDef;
                 }
                 else
                 {
-                    outRenderTarget.imageResource = std::get<RenderTargetNode>(nodes.at(renderTargetThatIsNotBeingUsed)).imageResource;
-                    renderTargetUseCount.at(renderTargetThatIsNotBeingUsed)++;
+                    outRenderTarget.imageResource.name = std::get<RenderTargetNode>(nodes.at(imageThatIsNotBeingUsed)).imageResource.name;
+                    imageUseCount.at(imageThatIsNotBeingUsed)++;
                 }
             }
 
             _print();
 
-            for(auto & outputName : N.outputNodes)
+            for(auto & outTarget : N.outputRenderTargets)
             {
                 //imageUseCount[nodes.at(outputName).imageResource]--;
-                renderTargetUseCount.at(outputName)--;
+                imageUseCount.at(outTarget.name)--;
             }
-            for(auto & inputName : N.inputNodes)
+            for(auto & inTarget : N.inputRenderTargets)
             {
                 //imageUseCount[nodes.at(inputName).imageResource]--;
-                renderTargetUseCount.at(inputName)--;
+                imageUseCount.at(inTarget.name)--;
             }
         }
     }
@@ -272,9 +285,9 @@ public:
     //    {
     //        auto nn =
     //        fmt::format("({}) --> {} --> {}",
-    //                       fmt::join(N.inputNodes.begin(), N.inputNodes.end(), ","),
+    //                       fmt::join(N.inputRenderTargets.begin(), N.inputRenderTargets.end(), ","),
     //                       name,
-    //                       fmt::join(N.outputNodes.begin(), N.outputNodes.end(), ",")
+    //                       fmt::join(N.outputRenderTargets.begin(), N.outputRenderTargets.end(), ",")
     //                    );
     //        spdlog::info("{}", nn);
     //    }
@@ -288,34 +301,7 @@ public:
         return std::get<RenderPassNode>(nodes[name]);
     }
 
-    RenderPassNode& createRenderPass(std::string const & name,
-                           std::vector<std::string> const & outputTargets,
-                           std::vector<std::string> const & inputTargets = {})
-    {
-        auto & N = createRenderPass(name);
-        N.setInputs(inputTargets);
-        N.setOutputs(outputTargets);
-//        for(auto & o :outputTargets)
-//        {
-//            N.insertOutputNode(o);
-//        }
-//        for(auto & i : inputTargets)
-//        {
-//            N.insertInputNode(i);
-//        }
-
-        return N;
-    }
-
-
-    struct Image
-    {
-        std::string name;
-        uint32_t width  = 0;
-        uint32_t height = 0;
-    };
-
-    std::map< std::string, Image> m_images;
+    std::map< std::string, ImageDefinition> m_images;
     std::map<std::string, node> nodes;
 };
 
@@ -323,7 +309,29 @@ int main(int argc, char ** argv)
 {
     FrameGraph G;
 
+#if 1
+    G.createRenderPass("ShadowPass")
+     .output("SM", VK_FORMAT_D32_SFLOAT);
+
+    G.createRenderPass("geometryPass")
+     .input("SM")
+     .output("C1", VK_FORMAT_R8G8B8A8_UNORM);
+
+    G.createRenderPass("HBlur1")
+     .input("C1")
+     .output("B1h", VK_FORMAT_R8G8B8A8_UNORM);
+
+    G.createRenderPass("VBlur1")
+     .input("B1h")
+     .output("B1v",VK_FORMAT_R8G8B8A8_UNORM);
+
+    G.createRenderPass("Final")
+     .input("C1")
+     .input("B1v")
+     .output("F", VK_FORMAT_R8G8B8A8_UNORM);
+#else
     G.createRenderPass("G", {"C1"});
+
 
     G.createRenderPass("H", {"C2"}, {"C1"});
 
@@ -334,24 +342,16 @@ int main(int argc, char ** argv)
     G.createRenderPass("Q", {"C5"}, {"C4"});
 
     G.createRenderPass("R", {"C6"}, {"C5", "C7"});
-
-//    G.generateImages();
-//
-//    G.printGraph();
-
-//    auto endNodes = G.findEndNodes();
-//    spdlog::info("End Nodes: {}", fmt::format("{}", fmt::join(BE(endNodes), ",")));
-//
-//    auto order = G.findExecutionOrder();
-//    spdlog::info("Execution Order: {}", fmt::format("{}", fmt::join(BE(order), ",")));
-
+#endif
     G.allocateImages();
 
+    auto order = G.findExecutionOrder();
+    spdlog::info("Order: {}", fmt::join(BE(order),","));
     for(auto & n : G.nodes)
     {
         if( std::holds_alternative<RenderTargetNode>(n.second) )
         {
-            spdlog::info("{} is using image: {}", n.first, std::get<RenderTargetNode>(n.second).imageResource);
+            spdlog::info("{} is using image: {}", n.first, std::get<RenderTargetNode>(n.second).imageResource.name);
         }
     }
     return 0;
